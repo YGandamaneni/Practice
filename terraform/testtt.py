caebd9761f695ac3000521ff79aad94a83158ac9
YGandamaneni <143759037+YGandamaneni@users.noreply.github.com>
2023-12-13 18:19:46 -0600

    Update testtt.py

--- terraform/testtt.py
+++ terraform/testtt.py
@@ -1,38 +1,42 @@
 from selenium import webdriver
-import time
-from time import sleep
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+
+chrome_options = webdriver.ChromeOptions()    
+# Add your options as needed    
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
+
+    
+driver = webdriver.Chrome(options = chrome_options)
+driver.get('http://github.com')
+print(driver.title)
+driver.quit()
 
 
-driver = webdriver.Chrome()
-
-#date_string = f'{datetime.now():%Y-%m-%d %H:%M:%S%z}'
-
-web_ui_url="https://python.org"
-near_url= "https://dgeo-scaleout-dev.s3.amazonaws.com/synthetic-data/small/seismicCubes_Runsum__15_degrees_2022.55539891/"
-mid_url = "https://dgeo-scaleout-dev.s3.amazonaws.com/synthetic-data/small/seismicCubes_Runsum__25_degrees_2022.55539891/"
-far_url = "https://dgeo-scaleout-dev.s3.amazonaws.com/synthetic-data/small/seismicCubes_Runsum__35_degrees_2022.55539891/"
-output_foldername = "selenium_test97"
-bucket_name = "dgeo-scaleout-dev"
-
-
-driver.get(web_ui_url)
-
-sleep(3)
-
-#near_box=driver.find_element("id", "near")
-#mid_box=driver.find_element("id", "mid")
-#far_box=driver.find_element("id", "far")
-
-#workflow_name_box=driver.find_element("id","workflow-name")
-
-#try:
-   # near_box.send_keys(near_url)
-   # sleep(1)
-    #mid_box.send_keys(mid_url)
-   # sleep(1)
-  ## sleep(1)
-    #workflow_name_box.send_keys("selenium_test_22")
-    #sleep(1)
 
    
 #finally:
