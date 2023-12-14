336bc24b096ad8e8bde9f6c896ecd0cb9a3f466d
YGandamaneni <143759037+YGandamaneni@users.noreply.github.com>
2023-12-13 18:00:33 -0600

    Update testtt.py

--- terraform/testtt.py
+++ terraform/testtt.py
@@ -1,15 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 import time
 from time import sleep
-from datetime import datetime
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver = webdriver.Chrome()
 
 #date_string = f'{datetime.now():%Y-%m-%d %H:%M:%S%z}'
 
